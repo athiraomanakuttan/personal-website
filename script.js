7b299bec10c876ec9883937fc0180f6935ff4044
athira <athiraomanakuttan20@gmail.com>
2024-04-04 07:14:35 +0530

    validation compleated

--- script.js
+++ script.js
@@ -21,7 +21,24 @@ contactForm.addEventListener('submit',(event)=>{
         alert("Your Email Id is not in a required format!");
         return false;
     }
-    return false;
+    else
+    {
+        console.log("SUccess");
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycby1CtFDMXpZBU5L9_HHBiXny69-e4S3cWNCm0gbGK3hn9xmDRxAUbVxF5NNer3AyRfQgw/exec",
+            data:$("#submit-form").serialize(),
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+    
+            }
+        })
+    }
 })
     
 
@@ -29,25 +46,8 @@ nameValidation = userNameVal=>{
     var pattern = /^[a-zA-Z]{3,}(?:[\s-][a-zA-Z]{3,})*$/;
    return pattern.test(userNameVal);
 }
-emailValidation= userEmailVlaue=>{
-    var pattern=/^(?!.*@.*@)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    return pattern.test(userEmailVlaue)
+emailValidation = userEmailValue => {
+    var pattern = /^(?!.*\.\d)(?=[a-zA-Z0-9._%+-]*[a-zA-Z]{3,}\d*@)[a-zA-Z0-9._%+-]+@[a-z]{3,}\.[a-z]{2,}$/i
+    return userEmailValue.match(pattern)
 }
-// $("#submit-form").submit((e)=>{
-//     alert("inside")
-//     e.preventDefault()
-//     $.ajax({
-//         url:"https://script.google.com/macros/s/AKfycby1CtFDMXpZBU5L9_HHBiXny69-e4S3cWNCm0gbGK3hn9xmDRxAUbVxF5NNer3AyRfQgw/exec",
-//         data:$("#submit-form").serialize(),
-//         method:"post",
-//         success:function (response){
-//             alert("Form submitted successfully")
-//             window.location.reload()
-//             //window.location.href="https://google.com"
-//         },
-//         error:function (err){
-//             alert("Something Error")
 
-//         }
-//     })
-// })
